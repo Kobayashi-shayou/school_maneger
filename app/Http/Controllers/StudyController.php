ba50b40b2603cf018cb6bfc3e15051b193a06abc
Kobayashi-shayou <0609mucc@gmail.com>
2022-12-02 16:35:00 +0900

    クーポン利用の実装完了

--- app/Http/Controllers/StudyController.php
+++ app/Http/Controllers/StudyController.php
@@ -25,9 +25,18 @@ class StudyController extends Controller
 		return view('user.apply', compact('study'));
 	}
 
-    public function confirm($id)
+    public function confirm(Request $request)
     {
+        $price = 5000;
+        $coupon = $request->coupon;
+        if ($coupon == 777)
+        {
+            //勉強会の価格、一律5000円からクーポン番号に対応した価格を値引く
+            // 777 -> １０００円分
+            $price = 5000 - 1000;
+        } 
+        $id = $request->study_id;
         $study = Study::find($id);
-		return view('user.course', compact('study'));
+		return view('user.confirm', compact('study', 'price'));
 	}
 }
---
+++ resources/views/user/confirm.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.app')
+@section('content')
+
+<div class="container">
+<div class="row">
+<div class="col-md-8 col-md-offset-2">
+<div class="panel panel-default">
+@include('layouts.session.alert_msg')
+<div class="panel-heading"><h3>コース申し込み確認フォーム</h3></div>
+
+<table class="table table-striped table-hover table-centered">
+<thead>
+<tr>
+<th>タイトル</th>
+<th>内容</th>
+<th>インストラクター</th>
+<th>価格</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<td>{{ $study->title }}</td>
+<td>{{ $study->body }}</td>
+<td>{{ $study->instructor_id }}</td>
+<td>{{ $price }} 円</td>
+</tr>
+</tbody>
+</table>
+
+
+
+<br>
+<a href="{{ route('user.confirm', $study->id) }}" class="btn btn-primary btn-sm">決定</a>
+<br>
+<br>
+
+
+</body>
+<a href="{{ route('user.home') }}">TOPへ戻る</a>
+
+</div>
+</div>
+</div>
+</div>
+
+@endsection
