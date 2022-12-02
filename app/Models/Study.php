f5d3663cb19e59cb95bfd6b60732e4940c1aa89f
Kobayashi-shayou <0609mucc@gmail.com>
2022-12-02 14:15:58 +0900

    リレーションの設定

--- app/Models/Study.php
+++ app/Models/Study.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Study extends Model
 {
     use HasFactory;
+
+	//リレーションコード
+        public function user() {
+                return $this->belongsTo('App\Models\User', 'user_id');
+        }
+
+        public function Instructor() {
+                return $this->belongsTo('App\Models\Instructor', 'instructor_id');
+        }
 }
