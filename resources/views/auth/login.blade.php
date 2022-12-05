2f7419e59bf2fc8ec8857219095dbd27f3747719
Kobayashi-shayou <0609mucc@gmail.com>
2022-12-05 21:30:01 +0900

    開発用ログインボタン作成

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -56,7 +56,11 @@
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Login') }}
                                 </button>
-
+<br>
+<br>
+<button type="button" class="btn btn-primary" onclick="location.href='{{ route('admin.top') }}'">
+{{ __('開発ログインボタン') }} 
+</button>
                                 @if (Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
                                         {{ __('Forgot Your Password?') }}
